e0717ac0a325efdc817182d441431be68e38a022
teuzowebdeveloper9 <mateussoftwaredeveloper@gmail.com>
2025-12-29 13:09:25 -0300

    chore: setup

---
+++ src/main/java/uber_test/tecnical/TecnicalApplication.java
@@ -0,0 +1,13 @@
+package uber_test.tecnical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecnicalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TecnicalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uber_test/tecnical/TecnicalApplicationTests.java
@@ -0,0 +1,13 @@
+package uber_test.tecnical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TecnicalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
