68b2f6b669c7e9e65b8c35e961687ed141ae2a19
teuzowebdeveloper9 <mateussoftwaredeveloper@gmail.com>
2025-12-29 17:09:43 -0300

    feat: add email sending endpoint to EmailSenderController

--- src/main/java/uber_test/tecnical/Controllers/EmailSenderController.java
+++ src/main/java/uber_test/tecnical/Controllers/EmailSenderController.java
@@ -1,10 +1,18 @@
 package uber_test.tecnical.Controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import uber_test.tecnical.aplication.EmailSenderService;
+import uber_test.tecnical.core.DTOs.EmailSenderDTO;
+import uber_test.tecnical.core.exceptions.EmailServiceException;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 @RequestMapping("/api/v1/email")
@@ -17,5 +25,21 @@ public class EmailSenderController {
         this.emailSenderService = emailSenderService;
     }
 
+
+    @PostMapping("/send")
+    public ResponseEntity<String> SendEmail(@RequestBody EmailSenderDTO request ) {
+        try{
+
+            this.emailSenderService.SendEmail(request.To() , request.Subject() , request.Body());
+
+            return ResponseEntity.ok("email sent successfully");
+
+            
+        }catch(EmailServiceException e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error while sending email");
+        }
+    }
+  
+    
 
 }
---
+++ src/main/java/uber_test/tecnical/core/DTOs/EmailSenderDTO.java
@@ -0,0 +1,6 @@
+package uber_test.tecnical.core.DTOs;
+
+public record EmailSenderDTO(String To, String Subject, String Body) {
+
+    
+}
